a94e653f20f3b691231b4053d01db6f24213be06
George Wortmann <gf.wortmann@gmail.com>
2022-11-30 21:03:15 +0300

    task 50 committed 1

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
-﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-//m = 3, n = 4.
-//0,5 7 -2 -0,2
-//1 -3,3 8 -9,9
-//8 7,8 -7,1 9
+﻿//Задача 50. Напишите программу, которая на вход принимает число и ищет его в двумерном массиве. Программа должна возвращать значение позиции (номер строки и столбца) этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
 
 
 
-
-double RandomDouble (double min, double max)
+int [,] FillArray (int rows, int columns, int min, int max)
 {
-    Random Buffer  = new Random();
-    double result = (Buffer.NextDouble() - 0.5) * (max - min);
-    return result;   
-}
-
-double [,] FillArray (int rows, int columns, double min, double max)
-{
-    double [,] result = new double [rows, columns];
+    int [,] result = new int [rows, columns];
+    Random k = new Random();
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
         {
-            result [i,j] = RandomDouble (min, max);
+           result [i,j] = k.Next(min, max);
         }
     }
     return result;
 }
 
 
-void WriteArray ( double [,] Array)
+void WriteArray ( int [,] Array)
 {
     int rows = Array.GetUpperBound(0)+1;
     int columns = Array.Length / rows;
@@ -39,22 +34,56 @@ void WriteArray ( double [,] Array)
         Console.WriteLine("\n");
         for (int j = 0; j < columns; j++)
         {
-            Console.Write($" {Array [i, j]:f2} ");
+            Console.Write($" {Array [i, j]} ");
         }
     }
     Console.WriteLine("\n");
 }
 
+int [] FindNumberPosition (int [,] Array, int target)
+{
+    int [] result = new int [2];
+    int rows = Array.GetUpperBound(0)+1;
+    int columns = Array.Length / rows;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for ( int j = 0; j < columns; j++)
+        {
+            if (target == Array [i, j])
+            {
+                result [0] = i;
+                result [1] = j;
+                return result;
+            }
+        }
+    }
+    result [0] = -1;
+    result [1] = -1;
+    return result;
+}
+
+
 int columns, rows;
 
 Console.Clear();
+
 Console.WriteLine($"Enter number of columns: ");
 columns = int.Parse(Console.ReadLine()!);
+
 Console.WriteLine($"Enter number of rows: ");
 rows = int.Parse(Console.ReadLine()!);
 
-double [,] Array = FillArray(rows, columns, -100, 100);
+int [,] Array = FillArray(rows, columns, -100, 100);
+
 WriteArray (Array);
 
-//Console.WriteLine($"Random number is: {RandomDouble(-100, 100)}");
+Console.WriteLine($"Enter the target number: ");
+int target = int.Parse(Console.ReadLine()!);
+
+int [] indexOfNumber = FindNumberPosition( Array, target );
+
+if (indexOfNumber[0] == -1) Console.WriteLine($"There no such number in the array.");
+else Console.WriteLine($"The target number is in the row {indexOfNumber[0]+1}, column {indexOfNumber[1]+1}");
+
 
